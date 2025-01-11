e9dba19f942fb34b5e1308b0240a7de9ff1eef45
kevyan24 <yan24010@byui.edu>
2025-01-11 14:36:22 -0500

    feat: add exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string answer = "y";
+
+        while (answer == "y") {
+            int magicNumber = new Random().Next(1, 100);
+            int guess = 0;
+            int attempts = 0;
+
+            while (guess != magicNumber) {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (guess < magicNumber) {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber) {
+                    Console.WriteLine("Lower");
+                }
+            }
+            Console.WriteLine($"You guessed it in {attempts} tries"); 
+
+            Console.Write("Do you want to play again? (y/n) ");
+            answer = Console.ReadLine().ToLower();  
+        }
+        Console.WriteLine("Goodbye!");
     }
 }
