0d77c7edac80a55e0b70b1580640c26da2c0b49d
kevyan24 <yan24010@byui.edu>
2025-01-10 21:13:48 -0500

    feat: add exercise 1 and 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Runtime.ConstrainedExecution;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your percentage? ");
+        string percentage = Console.ReadLine();
+        int percentageInt = int.Parse(percentage);
+
+        string letter;
+
+        if (percentageInt >= 90) {
+            letter = "A";
+        }
+        else if (percentageInt >= 80) {
+            letter = "B";
+        }
+        else if (percentageInt >= 70) {
+            letter = "C";
+        }
+        else if (percentageInt >= 60) {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+
+        string gradeSign;
+
+        if (percentageInt % 10 >= 7 && !(letter == "A" || letter == "F")) {
+            gradeSign = "+";
+        }
+        else if (percentageInt % 10 < 3 && !(letter == "F")) {
+            gradeSign = "-";
+        }
+        else {
+            gradeSign = "";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{gradeSign}");
+        
+        if (percentageInt >= 70) {
+            Console.WriteLine("You passed");
+        }
+        else {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
